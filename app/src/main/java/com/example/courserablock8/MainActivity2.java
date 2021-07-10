5a0eb9b4cdd23d79b9f9758ae52f228ea71c6a7f
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-10 08:41:31 +0530

    Set the text of TextView instance

--- app/src/main/java/com/example/courserablock8/MainActivity2.java
+++ app/src/main/java/com/example/courserablock8/MainActivity2.java
@@ -14,5 +14,6 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         float rating = getIntent().getFloatExtra("Value", 0);
         TextView textView = findViewById(R.id.text);
+        textView.setText(String.format("Welcome to second activity. Your rating is: %s", rating));
     }
 }
